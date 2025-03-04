aadc2fcc2ec666cb4724ebf85bca74fc16f4633c
deenoloo <doreenoloo2@gmail.com>
2025-03-04 06:40:45 -0500

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+const multiply = num1 * num2;
+
+let num3=22;
+let num4=6;
+const mod = num3 % num4;
+
+const max = Math.max(14, 12, 15,20);
+
+const random= Math.floor(Math.random()*100)+1;
